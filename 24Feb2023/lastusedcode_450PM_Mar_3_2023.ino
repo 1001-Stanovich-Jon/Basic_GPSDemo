a8722bcef1c3e2ae06f5c164e029e8de9f9c2441
1001-Stanovich-Jon <84261577+1001-Stanovich-Jon@users.noreply.github.com>
2023-03-03 16:50:54 -0800

    Create lastusedcode_450PM_Mar_3_2023.ino

---
+++ 24Feb2023/lastusedcode_450PM_Mar_3_2023.ino
@@ -0,0 +1,128 @@
+
+#include <SPI.h>
+#include <SD.h>
+#include <SoftwareSerial.h>
+
+#define SDCS 10
+#define GPSTX 3
+#define GPSRX 2
+
+SoftwareSerial GPSSerial(GPSTX, GPSRX);
+#define chipSelect 10
+#define maxMsg 151
+
+#define errorLED 5
+#define standbyLED 6
+#define workingLED 7
+
+#define stopPin 9
+
+#define indLED 1
+
+#define GPSBaudRate 9600
+
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  if (indLED) {
+    pinMode(errorLED, OUTPUT);
+    pinMode(standbyLED, OUTPUT);
+    pinMode(workingLED, OUTPUT);
+    pinMode(stopPin, INPUT);
+  
+    digitalWrite(errorLED, 0);
+    digitalWrite(standbyLED, 1);
+    digitalWrite(workingLED, 0);
+  } 
+  
+  if (!SD.begin(chipSelect)) {
+    // If SD card can't be initialized, blink error LED
+    if (indLED) {
+      while(1) {
+        digitalWrite(errorLED, 1);
+        delay(1000);
+        digitalWrite(errorLED, 0);
+        delay(1000);
+      }
+    }
+
+    // otherwise just stall
+    while (1);
+  }
+
+  GPSSerial.begin(GPSBaudRate);
+
+  delay(5000);
+
+  if (indLED) {
+    digitalWrite(standbyLED, 0);
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int stopSignal = digitalRead(stopPin);
+
+
+  static int loopCount = 0;
+
+    
+  char gpsChar[151];
+
+
+  if (!stopSignal){
+    File dataFile;
+    char fname[] = "dlf_U.txt";
+    dataFile = SD.open(fname, FILE_WRITE);
+    int charCount = 0;
+    while (GPSSerial.available() && charCount < 151) {
+      
+      gpsChar[charCount] = GPSSerial.read();
+      charCount++;
+    }
+    gpsChar[charCount] = '\0';
+    loopCount++;
+
+    dataFile.println(gpsChar);
+    dataFile.close();
+
+    
+    delay(1000);
+  }
+  
+  else {
+    if (indLED) {
+      while(1) {
+        digitalWrite(errorLED, 1);
+        delay(100);
+        digitalWrite(errorLED, 0);
+        delay(2000);
+        digitalWrite(standbyLED, 1);
+        delay(100);
+        digitalWrite(standbyLED, 0);
+        delay(2000);
+      }
+    }
+    while (1);
+  }
+
+  if (indLED) {
+    digitalWrite(workingLED, 1);
+    delay(200);
+    digitalWrite(workingLED,0);
+  }
+
+  if (stopSignal) {
+    digitalWrite(errorLED, 0);
+    digitalWrite(workingLED, 0);
+    while(1) {
+      delay(500);
+      digitalWrite(standbyLED, 0);
+      delay(500);
+      digitalWrite(standbyLED,1);
+    }
+  }
+
+}
